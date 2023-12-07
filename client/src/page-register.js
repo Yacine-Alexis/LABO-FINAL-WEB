8b6b420617c88aa4b19ac1144d0351a79b98e39a
Yacine <yacinealexis@outlook.fr>
2023-12-07 18:12:15 -0500

    12

--- client/src/page-register.js
+++
@@ -1,7 +0,0 @@
-import {register} from './chat-api';
-
-window.addEventListener("load", () => {
-    document.querySelector("form").onsubmit = function () {
-        return register(this);
-    }
-})
